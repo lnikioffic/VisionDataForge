596a0a8ca8e4064c6b9b97791b911f8c118dd827
lnikioffic <nik_ivanov_33@mail.ru>
2024-06-08 15:01:01 +0500

    add custom auth

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,46 +1,33 @@
-from typing import Annotated, Optional, Tuple
-from fastapi import Depends, HTTPException, Request, status, Form
-from fastapi.responses import RedirectResponse
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends, HTTPException, status, Form
 from jwt.exceptions import InvalidTokenError
-from starlette.status import HTTP_401_UNAUTHORIZED
 
+from src.auth.oauth import OAuthCustom
 from src.auth.token import (
-    create_access_token, 
-    create_refresh_token, 
-    TOKEN_TYPE_FIELD, 
-    ACCESS_TOKEN_TYPE, 
-    REFRESH_TOKEN_TYPE
+    create_access_token,
+    create_refresh_token,
+    TOKEN_TYPE_FIELD,
+    ACCESS_TOKEN_TYPE,
+    REFRESH_TOKEN_TYPE,
 )
 from src.users.dependencies import valid_user_id, valid_user_username
-from src.users.schemas import UserLogin, UserRead
+from src.users.schemas import UserRead
 from src.auth.schemas import TokenInfo, AuthForm
 from src.auth import utils as auth_utils
 from src.users.service import UserService
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/token')
 
-async def get_current_token_from_cookies(request: Request) -> Optional[str]:
-        token = request.cookies.get("access_token")
-        if not token:
-            token = request.cookies.get("refresh_token")
-            if not token:
-                raise HTTPException(
-                    status_code=HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
-        return token
+oauth = OAuthCustom(tokenUrl='/auth/token')
 
 
-async def get_current_token_payload(token: Annotated[str, Depends(get_current_token_from_cookies)]) -> UserRead:
+async def get_current_token_payload(token: Annotated[str, Depends(oauth)]) -> UserRead:
     try:
         payload = auth_utils.decode_jwt(token=token)
     except InvalidTokenError as ex:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-            headers={"WWW-Authenticate": "Bearer"},
+            detail='Invalid token',
+            headers={'WWW-Authenticate': 'Bearer'},
         )
     return payload
 
@@ -54,115 +41,112 @@ async def validate_token_type(
         return True
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=f"invalid token type {current_token_type!r} expected {token_type!r}",
+        detail=f'invalid token type {current_token_type!r} expected {token_type!r}',
     )
+
 
 async def get_user_by_token_sub(
-        payload: Annotated[dict, Depends(get_current_token_payload)],
-        service: Annotated[UserService, Depends()]
-    ) -> UserRead:
-    id: str | None = payload.get("sub")
+    payload: Annotated[dict, Depends(get_current_token_payload)],
+    service: Annotated[UserService, Depends()],
+) -> UserRead:
+    id: str | None = payload.get('sub')
     if user := await valid_user_id(int(id), service):
         return user
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="token invalid (user not found)",
+        detail='token invalid (user not found)',
     )
-    
+
 
 async def get_current_auth_user(
-        payload: Annotated[dict, Depends(get_current_token_payload)],
-        service: Annotated[UserService, Depends()]
-    ) -> UserRead:
+    payload: Annotated[dict, Depends(get_current_token_payload)],
+    service: Annotated[UserService, Depends()],
+) -> UserRead:
     await validate_token_type(payload=payload, token_type=ACCESS_TOKEN_TYPE)
-    id: str | None = payload.get("sub")
+    id: str | None = payload.get('sub')
     if user := await valid_user_id(int(id), service):
         return user
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="token invalid (user not found)",
+        detail='token invalid (user not found)',
     )
 
 
 async def get_current_auth_user_for_refresh(
-        payload: Annotated[dict, Depends(get_current_token_payload)],
-        service: Annotated[UserService, Depends()]
-    ) -> UserRead:
+    payload: Annotated[dict, Depends(get_current_token_payload)],
+    service: Annotated[UserService, Depends()],
+) -> UserRead:
     await validate_token_type(payload=payload, token_type=REFRESH_TOKEN_TYPE)
-    id: str | None = payload.get("sub")
+    id: str | None = payload.get('sub')
     if user := await valid_user_id(int(id), service):
         return user
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="token invalid (user not found)",
+        detail='token invalid (user not found)',
     )
 
 
 async def get_current_active_auth_user(
     user: Annotated[UserRead, Depends(get_current_auth_user)]
 ) -> UserRead:
-    
+
     if user.is_active:
         return user
-    
+
     raise HTTPException(
         status_code=status.HTTP_403_FORBIDDEN,
-        detail="user inactive",
+        detail='user inactive',
     )
 
 
 async def validate_auth_user(
-        #data_form: Annotated[AuthForm, Depends()],
-        username: Annotated[str, Form()],
-        password: Annotated[str, Form()],
-        service: Annotated[UserService, Depends()]
-    ) -> TokenInfo:
+    # data_form: Annotated[AuthForm, Depends()],
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    service: Annotated[UserService, Depends()],
+) -> TokenInfo:
     unauthed_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="invalid username or password",
+        detail='invalid username or password',
         headers={'WWW-Authenticate': 'Bearer'},
     )
     user: UserRead = await valid_user_username(username, service)
     if not user:
         raise unauthed_exc
-    
+
     if not auth_utils.validate_password(
         password=password,
         hash_password=user.hashed_password,
     ):
         raise unauthed_exc
-    
+
     return await create_token_jwt(user)
 
 
 async def validate_create_user(
-        user: UserRead,
-    ) -> TokenInfo:
+    user: UserRead,
+) -> TokenInfo:
 
     return await create_token_jwt(user)
+
+    '''
+    Создание токенов
+    '''
 
 
 async def create_token_jwt(user: UserRead) -> TokenInfo:
     access_token = await create_access_token(user)
     refresh_token = await create_refresh_token(user)
 
-    return TokenInfo(
-        access_token=access_token,
-        refresh_token=refresh_token
-    )
+    return TokenInfo(access_token=access_token, refresh_token=refresh_token)
 
 
 async def refresh_token_jwt(user: UserRead) -> TokenInfo:
     access_token = await create_access_token(user)
 
-    return TokenInfo(
-        access_token=access_token
-    )
+    return TokenInfo(access_token=access_token)
 
 
 async def delete_token_jwt(user: UserRead) -> TokenInfo:
 
-    return TokenInfo(
-        access_token='',
-        refresh_token=None
-    )
+    return TokenInfo(access_token='', refresh_token=None)
---
+++ src/auth/oauth.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+from fastapi import HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer
+
+
+def get_authorization_scheme_param(
+    authorization_header_value: str | None,
+) -> Tuple[str, str]:
+    if not authorization_header_value:
+        return '', ''
+    scheme, _, param = authorization_header_value.partition(' ')
+    return scheme, param
+
+
+class OAuthCustom(OAuth2PasswordBearer):
+    async def __call__(self, request: Request) -> str | None:
+        token = request.cookies.get('access_token')
+        if not token:
+            token = request.cookies.get('refresh_token')
+        scheme, param = get_authorization_scheme_param(token)
+        if not token or scheme.lower() != 'bearer':
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail='Not authenticated',
+                    headers={'WWW-Authenticate': 'Bearer'},
+                )
+            else:
+                return None
+        return param
